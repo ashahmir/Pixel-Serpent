de775776e1ced6e35aa7e0d56993bbe55b4654c0
Ghulam Latif <141661326+ashahmir@users.noreply.github.com>
2025-07-18 16:26:50 +0500

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,9 @@
 from turtle import Turtle
 from snake import Snake
 
+with open("score.txt", mode="r") as file:
+    score = file.read()
+
 
 class Scoreboard(Turtle):
     def __init__(self):
@@ -9,12 +12,23 @@ class Scoreboard(Turtle):
         self.color("white")
         self.hideturtle()
         self.score = 0
+        self.highScore = int(score)
         self.goto(0,280)
         self.update_score()
 
     def update_score(self):
         self.clear()
-        self.write(f"Score : {self.score}", align="center", font=("Arial", 14, "normal"))
+        self.write(f"Score: {self.score}           Highscore: {self.highScore}", align="center",
+                   font=("Arial", 14, "normal"))
+
+    def reset(self):
+        if self.score > self.highScore:
+            self.highScore = self.score
+            with open("score.txt", mode="w") as f:
+                f.write(str(self.highScore))
+
+        self.score = 0
+        self.update_score()
 
     def increase_score(self):
         self.score += 1
